9d0a805b0e4d6b894b9d959899e1499b883de1c4
Padoctb <yeahndima@gmail.com>
2018-08-14 18:07:21 +0300

    i packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './store/'
 import {Provider} from 'react-redux'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from "./tools/misc/scrollToTop.js"
 
-ReactDOM.render(<Provider store={store}><HashRouter><ScrollToTop><App/></ScrollToTop></HashRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><BrowserRouter><ScrollToTop><App/></ScrollToTop></BrowserRouter></Provider>, document.getElementById('root'));
